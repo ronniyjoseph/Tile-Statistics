8d5bc21d9f95d8728b305b7ec4abf62bb148e407
Ronniy Joseph <ronniy.joseph@icrar.org>
2019-08-22 18:42:48 +0800

    Rasterized plot, so 6e6 points make it less bloated

--- Stats_plotter.py
+++ Stats_plotter.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -10,8 +11,8 @@ from astropy.time import Time
 
 plot_name = "all_eor_data_stats.pdf"
 data = numpy.loadtxt("./statistics_output/All_EoR_Stats.txt")
-fontsize = 15
-
+fontsize = 30
+ticksize = 25
 #statistics = numpy.loadtxt("/home/ronniyjoseph/Downloads/mwa_health.csv", skiprows =1, delimiter = ',' )
 
 #dipole_stats = numpy.loadtxt("../broken_dipole_count_2015_PhaseI.txt")[1:]
@@ -25,7 +26,7 @@ years = gps_stamp.decimalyear
 
 print("Making plot")
 
-figure, axes  = pyplot.subplots( 1, 1, figsize = (25,5))
+figure, axes  = pyplot.subplots( 1, 1, figsize = (15,5), subplot_kw = dict(rasterized = True))
 figure.subplots_adjust(bottom=0.2)
 
 axes.scatter(years, data[:, 1], label = " Dipole", s= 10)#, "C0", label = "XX")
@@ -33,7 +34,8 @@ axes.scatter(years, data[:, 2], label = "2 Dipoles", s = 10)#, "C1", label = "YY
 
 axes.set_xlabel("Years", fontsize = fontsize)
 axes.set_ylabel("Number of Tiles", fontsize = fontsize)
-axes.legend(loc="upper right")
+axes.legend(loc="upper right", fontsize = ticksize)
+axes.tick_params(axis='both', which='major', labelsize=ticksize)
 
 figure.savefig("./statistics_plots/" + plot_name)
-pyplot.show()
+figure.tight_layout()
